07f7116358bf794966120e404871cb7ff3671205
lukamacho <lmach19@freeuni.edu.ge>
2021-08-20 10:33:38 +0400

    ChangedStyle

--- basicTextEditor/TextEditor.py
+++ basicTextEditor/TextEditor.py
@@ -12,12 +12,12 @@ def change_color():
 
 
 def change_fonts(*args):
-    text_area.config(font=(font_name.get(),size_box.get()))
+    text_area.config(font=(font_name.get(), size_box.get()))
 
 
 def new_file():
     window.title("Untitled")
-    text_area.delete(1.0,END)
+    text_area.delete(1.0, END)
 
 
 def open_file():
@@ -26,10 +26,10 @@ def open_file():
                                       ("Text documents" , "*.txt")])
     try:
         window.title(os.path.basename(file))
-        text_area.delete(1.0,END)
+        text_area.delete(1.0, END)
 
-        file = open(file,"r")
-        text_area.insert(1.0,file.read())
+        file = open(file, "r")
+        text_area.insert(1.0, file.read())
     except EXCEPTION:
         print("Couldn't open file")
     finally:
@@ -48,12 +48,13 @@ def save_file():
     else:
         try:
             window.title(os.path.basename(file))
-            file = open(file,"w")
-            file.write(text_area.get(1.0,END))
+            file = open(file, "w")
+            file.write(text_area.get(1.0, END))
         except EXCEPTION:
             print("Couldn't open file")
         finally:
             file.close()
+
 
 def cut():
     text_area.event_generate("<<Cut>>")
@@ -68,12 +69,11 @@ def paste():
 
 
 def about():
-   showinfo("About this program","This is one of the first python project written by me.")
+   showinfo("About this program", "This is one of the first python project written by me.")
 
 
 def quit():
     window.destroy()
-
 
 
 window = Tk()
@@ -87,7 +87,7 @@ screen_height = window.winfo_screenheight()
 x = int((screen_width/2) - (window_width/2))
 y = int((screen_height/2) - (window_height/2))
 
-window.geometry("{}x{}+{}+{}".format(window_width,window_height,x,y))
+window.geometry("{}x{}+{}+{}".format(window_width, window_height, x, y))
 
 font_name = StringVar(window)
 font_name.set("Arial")
@@ -95,48 +95,48 @@ font_name.set("Arial")
 font_size = StringVar(window)
 font_size.set("25")
 
-text_area = Text(window,font=(font_name.get(),font_size.get()))
+text_area = Text(window, font=(font_name.get(), font_size.get()))
 scroll_bar = Scrollbar(text_area)
-window.grid_rowconfigure(0,weight=1)
-window.grid_columnconfigure(0,weight=1)
-text_area.grid(sticky = N + E + S + W)
+window.grid_rowconfigure(0, weight=2)
+window.grid_columnconfigure(0, weight=2)
+text_area.grid(sticky=N + E + S + W)
 
 
 frame = Frame(window)
 frame.grid()
 
-color_button =  Button(frame, text ="color" , command = change_color)
-color_button.grid(row=0,column=0)
+color_button = Button(frame, text ="color", command=change_color)
+color_button.grid(row=0, column=0)
 
-font_box  = OptionMenu(frame,font_name,*font.families(),command=change_fonts)
-font_box.grid(row=0,column=1)
-size_box = Spinbox(frame,from_=1, to=100, textvariable=font_size,command =change_fonts)
-size_box.grid(row=0,column=2)
+font_box = OptionMenu(frame, font_name, *font.families(), command=change_fonts)
+font_box.grid(row=0, column=1)
+size_box = Spinbox(frame, from_=1, to=100, textvariable=font_size, command=change_fonts)
+size_box.grid(row=0, column=2)
 
 
 menu_bar = Menu(window)
 window.config(menu=menu_bar)
 
-file_menu = Menu(menu_bar,tearoff=0)
-menu_bar.add_cascade(label="File",menu=file_menu)
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="File", menu=file_menu)
 
-file_menu.add_command(label="New",command = new_file)
-file_menu.add_command(label="Open",command = open_file)
-file_menu.add_command(label="Save",command = save_file)
+file_menu.add_command(label="New", command=new_file)
+file_menu.add_command(label="Open", command=open_file)
+file_menu.add_command(label="Save", command=save_file)
 file_menu.add_separator()
-file_menu.add_command(label="Exit",command = quit)
+file_menu.add_command(label="Exit", command=quit)
 
-edit_menu = Menu(menu_bar,tearoff=0)
-menu_bar.add_cascade(label="Edit",menu=edit_menu)
-edit_menu.add_command(label="Cut",command=cut)
-edit_menu.add_command(label="Copy",command=copy)
-edit_menu.add_command(label="Paste",command=paste)
+edit_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Edit", menu=edit_menu)
+edit_menu.add_command(label="Cut", command=cut)
+edit_menu.add_command(label="Copy", command=copy)
+edit_menu.add_command(label="Paste", command=paste)
 
-help_menu = Menu(menu_bar,tearoff=0)
-menu_bar.add_cascade(label="Help",menu = help_menu)
-help_menu.add_command(label="About",command = about)
+help_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Help", menu=help_menu)
+help_menu.add_command(label="About", command=about)
 
-scroll_bar.pack(side=RIGHT, fill =Y)
+scroll_bar.pack(side=RIGHT, fill=Y)
 text_area.config(yscrollcommand=scroll_bar.set)
 
 window.mainloop()
